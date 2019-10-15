ac6c8ca934c055c3f0ed47364d13b94bb23c32d9
andrei <andreiv0202@gmail.com>
2019-10-16 04:27:27 +0900

    Changed Readmore and one tag

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"> 
-        <p>
+        <span>
           First Management system.
-        </p> 
+        </span> 
       </header>
     </div>
   );

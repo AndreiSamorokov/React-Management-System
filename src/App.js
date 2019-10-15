a839fa0bf41256f2845052f696b5a93e31e2c68d
andrei <andreiv0202@gmail.com>
2019-10-16 05:14:39 +0900

    create customer js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component, Fragment } from 'react'; 
+import Customer from './components/Customer';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"> 
-        <span>
-          First Management system.
-        </span> 
-      </header>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: 'Vlad',
+    birthday: '45',
+    gender: 'man',
+    job: 'CEO'
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: 'Andrei',
+    birthday: '35',
+    gender: 'man',
+    job: 'Developer'
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: 'Sasya',
+    birthday: '25',
+    gender: 'man',
+    job: 'Designer'
+  }
+]
+
+class App extends Component{
+  render(){
+    return(
+      <Fragment>
+        {
+          customers.map(c=>{return ( <Customer key = {c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) })
+        }
+      </Fragment>
+    )
+  }
 }
 
 export default App;

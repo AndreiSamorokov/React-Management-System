82508422e8ecd91d41d9464b862fd952e14dd8b1
andrei <andreiv0202@gmail.com>
2019-10-23 01:55:04 +0900

    Add Customer Loading view

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 import { withStyles } from "@material-ui/core/styles";
 
 import './App.css';
@@ -17,6 +19,9 @@ const style = theme => ({
   },
   talbe: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
@@ -24,16 +29,18 @@ const style = theme => ({
 
 class App extends Component{
   state = {
-    customers : ""
+    customers : "",
+    completed : 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
 
   }
-
+    
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
@@ -41,6 +48,11 @@ class App extends Component{
     return body;
   }
 
+  
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
   render(){
     const { classes } = this.props;
     return(
@@ -59,7 +71,14 @@ class App extends Component{
           <TableBody>
           {this.state.customers ? this.state.customers.map(c=>{
             return ( <Customer key = {c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) 
-          }) : ""}
+          }) : 
+          
+          <TableRow >
+            <TableCell colspan="6" align="center">
+              <CircularProgress className={classes.progress} value={this.state.completed} variant="determinate" />
+            </TableCell>
+          </TableRow>
+          }
           </TableBody>
         </Table>
       </Paper>

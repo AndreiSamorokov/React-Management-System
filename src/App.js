6cd85f759857f43105ddc0f8ddd4c4bdc2d833b2
andrei <andreiv0202@gmail.com>
2019-10-22 23:52:29 +0900

    Test Material Design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React,{ Component, Fragment } from 'react'; 
+import React,{ Component } from 'react'; 
 import Customer from './components/Customer';
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
 
 import './App.css';
+const style = theme => ({
+  root: {
+    with: '100%',
+    martinTop: theme.spacing(3),
+    overfloX: "auto"
+  },
+  talbe: {
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -32,14 +49,29 @@ const customers = [
 
 class App extends Component{
   render(){
+    const { classes } = this.props;
     return(
-      <Fragment>
-        {
-          customers.map(c=>{return ( <Customer key = {c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) })
-        }
-      </Fragment>
+      <Paper className={classes.root}>
+        <Table className={classes.talbe}>
+          <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell>Img</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Birthday</TableCell>
+              <TableCell>Gender</TableCell>
+              <TableCell>Job</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+            customers.map(c=>{return ( <Customer key = {c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) })
+          }
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
 
-export default App;
+export default withStyles(style)(App);
